4722b98328bea99f1a34a588fd55b9513861eee7
Jake Bailey <asaxplayinghorse@gmail.com>
2021-01-24 23:11:27 -0700

    Updates 20210124

--- copyNotes.py
+++ copyNotes.py
@@ -3,9 +3,27 @@
 
 import os
 import shutil
+import sys
 
-sourceDirectory = "/Volumes/Anonymous/OneDrive/Org/Slipbox"
-destinationDirectory = "/Users/jakebailey/Projects/NotesSite/content"
+print(len(sys.argv))
+
+if len(sys.argv) == 1:
+    switch = True
+else:
+    if sys.argv[1] == "-w":
+        switch = True
+    else:
+        switch = False
+
+if switch:
+    # We're on windows
+    sourceDirectory = "C:/Users/asaxp/OneDrive/Org/Slipbox"
+    destinationDirectory = "C:/Users/asaxp/OneDrive/Workspace/notesSite/content"
+else:
+    # We're on Mac
+    sourceDirectory = "/Users/jakebailey/OneDrive/Org/Slipbox"
+    destinationDirectory = "/Users/jakebailey/OneDrive/Workspace/notesSite/content"
+
 
 ## Clean destination
 for root, dires, files in os.walk(destinationDirectory):
@@ -15,4 +33,4 @@ for root, dires, files in os.walk(destinationDirectory):
 for root, dirs, files in os.walk(sourceDirectory):
     for f in files:
         if f.endswith(".md"):
-           shutil.copy2(os.path.join(root,f), os.path.join(destinationDirectory, f))
+            shutil.copy2(os.path.join(root,f), os.path.join(destinationDirectory, f))
